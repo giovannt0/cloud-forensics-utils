7cb35bc1f3f168a171fb808e277d50f3d58c9736
Theo Giovanna <gtheo@google.com>
2020-07-02 16:07:58 +0200

    Add StartAnalysisVm functionality
    Signed-off-by: Theo Giovanna <gtheo@google.com>

--- libcloudforensics/providers/azure/forensics.py
+++ libcloudforensics/providers/azure/forensics.py
@@ -14,10 +14,9 @@
 # limitations under the License.
 """Forensics on Azure."""
 
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Optional, List, Dict, Tuple
 
-from libcloudforensics.providers.azure.internal import account
-from libcloudforensics.providers.azure.internal import common
+from libcloudforensics.providers.azure.internal import account, common
 
 if TYPE_CHECKING:
   from libcloudforensics.providers.azure.internal import compute
@@ -25,6 +24,7 @@ if TYPE_CHECKING:
 
 def CreateDiskCopy(
     subscription_id: str,
+    resource_group_name: str,
     instance_name: Optional[str] = None,
     disk_name: Optional[str] = None,
     disk_type: str = 'Standard_LRS') -> 'compute.AZDisk':
@@ -32,6 +32,8 @@ def CreateDiskCopy(
 
   Args:
     subscription_id (str): The Azure subscription ID to use.
+    resource_group_name (str): The resource group in which to create the disk
+        copy.
     instance_name (str): Optional. Instance name of the instance using the
         disk to be copied. If specified, the boot disk of the instance will be
         copied. If disk_name is also specified, then the disk pointed to by
@@ -54,7 +56,7 @@ def CreateDiskCopy(
     raise ValueError(
         'You must specify at least one of [instance_name, disk_name].')
 
-  az_account = account.AZAccount(subscription_id)
+  az_account = account.AZAccount(subscription_id, resource_group_name)
 
   try:
     if disk_name:
@@ -79,3 +81,56 @@ def CreateDiskCopy(
     raise RuntimeError(error_msg)
 
   return new_disk
+
+
+def StartAnalysisVm(
+    subscription_id: str,
+    resource_group_name: str,
+    vm_name: str,
+    boot_disk_size: int,
+    cpu_cores: int = 4,
+    memory_in_mb: int = 8192,
+    attach_disks: Optional[List[str]] = None,
+    ssh_public_key: Optional[str] = None,
+    tags: Optional[Dict[str, str]] = None) -> Tuple['compute.AZVirtualMachine', bool]:  # pylint: disable=line-too-long
+  """Start a virtual machine for analysis purposes.
+
+  Look for an existing Azure virtual machine with name vm_name. If found,
+  this instance will be started and used as analysis VM. If not found, then a
+  new vm with that name will be created, started and returned. Note that if a
+  new vm is created, you should provide the ssh_public_key parameter.
+
+  Args:
+    subscription_id (str): The Azure subscription ID to use.
+    resource_group_name (str): The resource group in which to create the
+        analysis vm.
+    vm_name (str): The name for the virtual machine.
+    boot_disk_size (int): The size of the analysis VM boot disk (in GB).
+    cpu_cores (int): Number of CPU cores for the analysis VM.
+    memory_in_mb (int): The memory size (in MB) for the analysis VM.
+    attach_disks (List[str]): Optional. List of disk names to attach to the VM.
+    ssh_public_key (str): Optional. A SSH public key data to associate
+        with the VM. If none provided, then a newly created VM will not
+        be accessible.
+    tags (Dict[str, str]): Optional. A dictionary of tags to add to the
+        instance, for example {'TicketID': 'xxx'}. An entry for the instance
+        name is added by default.
+
+  Returns:
+    Tuple[AZVirtualMachine, bool]: a tuple with a virtual machine object
+        and a boolean indicating if the virtual machine was created or not.
+  """
+
+  az_account = account.AZAccount(subscription_id, resource_group_name)
+
+  analysis_vm, created = az_account.GetOrCreateAnalysisVm(
+      vm_name,
+      boot_disk_size,
+      cpu_cores,
+      memory_in_mb,
+      ssh_public_key=ssh_public_key,
+      tags=tags)
+
+  for disk_name in (attach_disks or []):
+    analysis_vm.AttachDisk(az_account.GetDisk(disk_name))
+  return analysis_vm, created
